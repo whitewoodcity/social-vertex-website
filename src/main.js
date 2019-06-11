8e1d09ea0b46828ee0119d6dc514f2d357808f3b
iBLOC.szz <874624842@qq.com>
2019-06-12 01:23:55 +0800

    :heavy_plus_sign: Add vuex.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import './plugins/axios'
 import App from './App.vue'
 import './plugins/ant-design-vue.js'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
