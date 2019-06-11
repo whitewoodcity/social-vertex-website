2ba7ccbf179bf1a1d814bd21e08405a320ed5060
iBLOC.szz <874624842@qq.com>
2019-06-12 01:22:18 +0800

    :heavy_plus_sign: Add vue-router.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,12 @@
 <template>
-  <a-locale-provider :locale="zh_CN">
-    <div id="app">
-      <img src="./assets/logo.png">
-      <div>
-        <p>
-          If Ant-Design-Vue is successfully added to this project, you'll see an
-          <code v-text="'<a-button>'"></code>
-          <code v-text="'<a-pagination>'"></code>
-          below
-        </p>
-        <a-button type="primary">Primary</a-button>
-        <a-pagination size="small" :total="50" showSizeChanger showQuickJumper />
-      </div>
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-  </a-locale-provider>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  data() {
-    return {
-      zh_CN,
-    }
-  },
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 import './plugins/ant-design-vue.js'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
