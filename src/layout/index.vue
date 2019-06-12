2fe084e529d6f6e7873eed70ed92a30a1c455d41
iBLOC.szz <874624842@qq.com>
2019-06-12 10:58:09 +0800

    :building_construction: 使用全局layout配合子路由实现通用layout。

---
+++ src/layout/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="layout">
+    <a-layout>
+      <a-layout-header class="layout-header">
+        <a-row align="bottom" justify="space-around" type="flex">
+          <a-col :span="6">
+            <router-link to="/">
+              <div class="layout-header-logo">
+                <img src="https://vertx.io/assets/logo-sm.png" alt height="58">
+              </div>
+            </router-link>
+          </a-col>
+          <a-col :span="8">
+            <a-menu mode="horizontal">
+              <a-menu-item>
+                <router-link to="/">
+                  <a-icon type="info"/>主页
+                </router-link>
+              </a-menu-item>
+              <a-menu-item>
+                <router-link to="/about">
+                  <a-icon type="flag"/>关于
+                </router-link>
+              </a-menu-item>
+              <a-menu-item>
+                <router-link to="/awesome">文档</router-link>
+              </a-menu-item>
+              <a-menu-item>
+                <router-link to="/awesome">Awesome-Vert.x</router-link>
+              </a-menu-item>
+            </a-menu>
+          </a-col>
+          <a-col :span="4">
+            <a-button-group>
+              <a-button icon="login" size="large">登录</a-button>
+              <a-button icon="thunderbolt" size="large" type="primary">注册</a-button>
+            </a-button-group>
+          </a-col>
+        </a-row>
+      </a-layout-header>
+      <a-layout-content>
+        <router-view/>
+      </a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.layout {
+}
+.layout-header {
+  background-color: #ffffff;
+}
+.layout-header-logo {
+}
+</style>
+
--- src/router.js
+++ src/router.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Layout from './layout'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Layout',
+      component: Layout,
+      children: [{
+        path: '/',
+        name: 'Home',
+        component: Home,
+      }, {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+      }]
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+
   ]
 })
