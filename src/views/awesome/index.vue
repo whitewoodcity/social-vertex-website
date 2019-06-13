fdd5c2b71ed519eb2345246133e3fbb36b08cb6c
iBLOC.szz <874624842@qq.com>
2019-06-13 13:52:55 +0800

    :construction: 新增Awesome Vertx页面。

---
+++ src/views/awesome/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="awesome">
+    <a-tabs size="large" >
+      <a-tab-pane tab="Tab 1" key="1">Content of Tab Pane 1</a-tab-pane>
+      <a-tab-pane tab="Tab 2" key="2" forceRender>Content of Tab Pane 2</a-tab-pane>
+      <a-tab-pane tab="Tab 3" key="3">Content of Tab Pane 3</a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.awesome {
+}
+</style>
