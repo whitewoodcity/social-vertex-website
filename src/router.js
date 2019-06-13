a7d496a9150c5fdd5560cc5b674e1fa12c32ca78
iBLOC.szz <874624842@qq.com>
2019-06-13 13:55:37 +0800

    :construction: 页面加入router。

--- src/router.js
+++ src/router.js
@@ -1,23 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Layout from './layout/index.vue'
+import Index from './views/index.vue'
+import Publish from './views/publish/index.vue'
+import Document from './views/document/index.vue'
+import Community from './views/community/index.vue'
+import Awesome from './views/awesome/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
+    path: '/',
+    name: 'Layout',
+    component: Layout,
+    children: [{
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+      name: 'Index',
+      component: Index,
+    }, {
+      path: '/publish',
+      name: 'Publish',
+      component: Publish,
+    }, {
+      path: '/document',
+      name: 'Document',
+      component: Document,
+    }, {
+      path: '/community',
+      name: 'Community',
+      component: Community,
+    }, {
+      path: '/awesome',
+      name: 'Awesome',
+      component: Awesome,
+    }]
+  }]
 })
