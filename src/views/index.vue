4d6f9a8f94a5fb16afa27520ece20383e68039ff
iBLOC.szz <874624842@qq.com>
2019-06-13 13:45:13 +0800

    :construction: 新增主页。

---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index">This is an index page</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style lang="less" scoped>
+.index {
+  
+}
+</style>
