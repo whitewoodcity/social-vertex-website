ce345c6f4ce61315b6811da171a756c412eb501f
iBLOC.szz <874624842@qq.com>
2019-06-13 13:55:11 +0800

    :construction: 修改主页为JS。

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,12 +2,12 @@
   <div class="index">This is an index page</div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
+<script>
+export default {
   
-})
+}
 </script>
+
 
 <style lang="less" scoped>
 .index {
