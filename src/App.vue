36f8b58714524fc1b6cc87b9ce1f8b9620dfb9e6
iBLOC.szz <874624842@qq.com>
2019-06-13 13:42:29 +0800

    :sparkles: 添加全局回到顶部功能。

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <nprogress-container></nprogress-container>
     <router-view/>
+    <a-back-top />
   </div>
 </template>
 
@@ -22,9 +23,7 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 #nprogress .bar {
