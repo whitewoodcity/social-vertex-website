c48767d8575a934b6f5a69ff10dc1930f11cc518
iBLOC.szz <874624842@qq.com>
2019-06-13 13:53:36 +0800

    :construction: 新增文档页面。

---
+++ src/views/document/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="document">
+    <iframe class="document-iframe" :src="src" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      src: "https://vertxchina.github.io/vertx-translation-chinese/"
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.document {
+  width: 100%;
+  height: 100%;
+}
+.document-iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
