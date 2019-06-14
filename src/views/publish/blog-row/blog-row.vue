73e93c3df13ed6d29aae611fa6db36ff6651eaa8
Luis <47078307@qq.com>
2019-06-14 14:44:28 +0800

    add purple line to the blog titles

--- src/views/publish/blog-row/blog-row.vue
+++ src/views/publish/blog-row/blog-row.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="blog-row">
-        <div class="blog-row-title"><h2>VS Code Vert.x Starter Extension</h2></div>
+        <div class="blog-row-title"><h2><a>VS Code Vert.x Starter Extension</a></h2></div>
         <p class="blog-post-meta">29th May 2019 by <a href="http://github.com/danielpetisme">danielpetisme</a></p>
         <article>
             <p>Hi Vert.x community! Today, we are really excited to announce the
@@ -23,7 +23,7 @@
 
 <style lang="less" scoped>
     .blog-row-title{
-        border-left: #6f338b;
-        border-left-width: 5px;
+        border-left: 5px solid #6f338b;
+        padding-left: 3px;
     }
 </style>
