b3ebe94db3e8e8e6e633acdab899ae45d165c18c
hulinjiang <hulinjiang@miaozhen.com>
2019-06-17 11:59:42 +0800

    :lipstick: update markdown-plugin
    - import code highlight css

--- src/plugins/markdown.js
+++ src/plugins/markdown.js
@@ -4,5 +4,7 @@ import Vue from 'vue';
 import VueSimpleMarkdown from 'vue-simple-markdown'
 // You need a specific loader for CSS files like https://github.com/webpack/css-loader
 import 'vue-simple-markdown/dist/vue-simple-markdown.css'
+// code-block highlight
+import 'highlight.js/styles/github-gist.css'
 
 Vue.use(VueSimpleMarkdown)
