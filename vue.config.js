6c254729410dd1d74059b6dff587afd7eac021f2
Luis <47078307@qq.com>
2019-06-19 11:25:25 +0800

    add dev/prod -mode env config

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,6 @@ function resolve(dir) {
 module.exports = {
     // publicPath: "/",
     //mod 为github，为gh-pages分支编译
-    // publicPath: process.env.VUE_BRANCHES === 'gh-pages' ? '/social-vertex-website/': '/social-vertex-website/dist/',
     publicPath: process.env.VUE_BRANCHES === 'gh-pages' ? '/social-vertex-website/': '/',
     // 输出目录
     outputDir: 'dist',
