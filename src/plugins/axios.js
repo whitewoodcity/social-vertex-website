b0ffb98e014bb2cbe0ddc6affeb413c22c39265e
Luis <47078307@qq.com>
2019-06-19 10:57:30 +0800

    tmp commit

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -10,7 +10,7 @@ import axios from "axios";
 
 let config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  baseURL:'/api',
+  baseURL: process.env.VUE_APP_AXIOS_BAESE,
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
