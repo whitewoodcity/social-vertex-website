8e0d578f5f1a279dd1376825db97090f69133ffe
Luis <47078307@qq.com>
2019-12-06 11:18:34 +0800

    add static comment elements

--- src/plugins/ant-design-vue.js
+++ src/plugins/ant-design-vue.js
@@ -2,7 +2,8 @@
 import Vue from 'vue'
 // import 'ant-design-vue/dist/antd.css';
 import '../antd-variables.less';
-import { Button,Layout,Row,Col,Menu,Icon,Tabs,Input,Select,Dropdown,List,Avatar,Form,Divider,BackTop,Modal,Upload,message,notification,Spin,Badge,LocaleProvider} from 'ant-design-vue';
+import { Button,Layout,Row,Col,Menu,Icon,Tabs,Input,Select,Dropdown,List,Avatar,
+    Form,Divider,BackTop,Modal,Upload,message,notification,Spin,Badge,LocaleProvider,Comment} from 'ant-design-vue';
 Vue.use(Button);
 Vue.use(Layout);
 Vue.use(Row);
@@ -23,5 +24,6 @@ Vue.use(Upload);
 Vue.use(Spin);
 Vue.use(Badge);
 Vue.use(LocaleProvider);
+Vue.use(Comment);
 Vue.prototype.$message = message;
 Vue.prototype.$notification = notification;
