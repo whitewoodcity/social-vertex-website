1ac8df52026f64f7bd5335cbe0b68d60af862b9c
Luis <47078307@qq.com>
2019-12-17 15:54:29 +0800

    add userBriefCache into localStorage and vuex

--- src/store.js
+++ src/store.js
@@ -7,6 +7,7 @@ const USER_INFO_LOCAL_STORAGE_KEY ='loggedInUserInfo';
 const PERSON_INDEX_USER_SESSION_STORAGE_KEY = "personalIndexFlag";
 const EDIT_ARTICLE_FLAG = "editArticleFlag";
 const EDIT_ARTICLE = "editArticle";
+const USER_CACHE = "userCache";
 export default new Vuex.Store({
   state: {
     //从localStorage中获取用户信息 如果有 则处于登录状态
@@ -15,16 +16,33 @@ export default new Vuex.Store({
     selfIndex:sessionStorage.getItem(PERSON_INDEX_USER_SESSION_STORAGE_KEY) == null ? false : sessionStorage.getItem(PERSON_INDEX_USER_SESSION_STORAGE_KEY) === "true",
     //用于标记是否为编辑状态的帖子
     editArticleFlag: sessionStorage.getItem(EDIT_ARTICLE_FLAG) == null ? false : sessionStorage.getItem(EDIT_ARTICLE_FLAG) === "true",
-    editArticle: sessionStorage.getItem(EDIT_ARTICLE) ? JSON.parse(sessionStorage.getItem(EDIT_ARTICLE)) : null
+    editArticle: sessionStorage.getItem(EDIT_ARTICLE) ? JSON.parse(sessionStorage.getItem(EDIT_ARTICLE)) : null,
+    userCache : localStorage.getItem(USER_CACHE) ? JSON.parse(localStorage.getItem(USER_CACHE)) : {}
   },
 
   getters:{
     loggedIn: (state) => {
       return state.loggedInUserInfo;
+    },
+    userBriefById : (state) => (id) => {
+      return state.userCache[id];
     }
   },
 
   mutations: {
+    addUserBriefToCache(state, userBrief){
+      let cacheJsonStr = localStorage.getItem(USER_CACHE);
+      if (!cacheJsonStr) {
+        //add user brief info to local-storage and vuex-store
+        let ub = {}; ub[userBrief.id] = userBrief;
+        localStorage.setItem(USER_CACHE,JSON.stringify(ub));
+      }else {
+        let cachedObj = JSON.parse(cacheJsonStr);
+        cachedObj[userBrief.id] = userBrief;
+        localStorage.setItem(USER_CACHE,JSON.stringify(cachedObj));
+      }
+      state.userCache[userBrief.id] = userBrief;
+    },
     //登录操作
     doLogin(state,userInfo){
       localStorage.setItem(USER_INFO_LOCAL_STORAGE_KEY,JSON.stringify(userInfo));
